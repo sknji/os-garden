898ba0c6c758ea725ce0e8066aee95a0bd1c2ed4
urijn <26629741+urijn@users.noreply.github.com>
2022-11-08 12:20:25 +0300

    vga abstraction

---
+++ blog_os/src/custom_test.rs
@@ -0,0 +1,16 @@
+use crate::{print, println};
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
